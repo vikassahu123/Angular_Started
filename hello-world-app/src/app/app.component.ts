0ed1830c195c5c1bd491536f72c92981015525b7
Vikassahu62 <vikassahu54927@gamil.com>
2025-03-07 11:20:30 +0530

    vikas sahu UC_2_hello_dataBinding

--- hello-world-app/src/app/app.component.ts
+++ hello-world-app/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello-world-app';
+  message: string= 'Hello from BridgeLabz!'; 
 }
